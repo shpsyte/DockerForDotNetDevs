0bc339b83bdaeaf14a62f776f87d7c2d81cd5d03
Luiz Borges <jose.iscosistemas@gmail.com>
2024-12-02 07:43:30 -0800

    adding github action

--- DockerAPI/DB/UpdateDB.sql
+++ DockerAPI/DB/UpdateDB.sql
@@ -1,20 +1,30 @@
-﻿CREATE DATABASE SimpleList
+﻿IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'SimpleList')
+BEGIN
+    CREATE DATABASE SimpleList
+END
 GO
-       
+
 USE SimpleList
 GO
 
-create table Podcast (
- id uniqueidentifier primary key default newid(),
- Title varchar(100) not null
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Podcast') AND type in (N'U'))
+BEGIN
+CREATE TABLE Podcast (
+                         id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+                         Title VARCHAR(100) NOT NULL
 )
+END
 GO
 
-insert into Podcast (Title)
-values ('.NET EP 01'),
-       ('LINQ - DO and Donts'),
-       ('SQL - Server 101'),
-       ('Docker 101')
-
+IF NOT EXISTS (SELECT 1 FROM Podcast)
+BEGIN
+INSERT INTO Podcast (Title)
+VALUES
+    ('.NET EP 01'),
+    ('LINQ - DO and Donts'),
+    ('SQL - Server 101'),
+    ('Docker 101')
+END
+GO
     
        
